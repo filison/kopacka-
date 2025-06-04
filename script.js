ac17d0bbe93ae8dbf76c4d052e412d6ed68ae651
filison <65769401+filison@users.noreply.github.com>
2025-06-04 16:22:52 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const toggle = document.querySelector('.menu-toggle');
-  const nav = document.querySelector('nav');
-
-  toggle.addEventListener('click', function () {
-    nav.classList.toggle('active');
-  });
-});
